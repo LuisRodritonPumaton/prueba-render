95cc6477b25e6fc8b0a604c611f6e05eaf3a8b6f
Luis Rodrigo Puma Herencia <U19314535@utp.edu.pe>
2023-03-17 09:38:43 -0500

    dd

--- src/main/java/com/gestion/apigestionentidad/ApiGestionEntidadApplication.java
+++ src/main/java/com/gestion/apigestionentidad/ApiGestionEntidadApplication.java
@@ -8,16 +8,6 @@ public class ApiGestionEntidadApplication {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGestionEntidadApplication.class, args);
-		
-		// Usuario user = new Usuario();
-        // user.setEmail("admin");
-        // user.setNombreUsuario("admin");
-        // user.setIdUsuario(Long.parseLong("1"));
-        // user.setPassword(new BCryptPasswordEncoder().encode("admin"));
-		// ApplicationContext ctx = SpringApplication.run(ApiGestionEntidadApplication.class, args);
-
-		// UsuarioRepository usuarioRepository = ctx.getBean(UsuarioRepository.class);
-		// usuarioRepository.save(user);
 	}
 
 }
